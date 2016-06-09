ddd5a8b9bca692c0372f16fbe90286f2487f6b95
Patrik Sandahl <kosmoskatten.software@gmail.com>
2016-06-09 23:28:37 +0200

    Open a named Tun device as a Handle.

--- lib/System/IO/Tun.c
+++ lib/System/IO/Tun.c
@@ -1,6 +1,4 @@
 
-#include <stdio.h>
-
 #include <string.h>
 
 #include <unistd.h>
@@ -13,12 +11,26 @@
 
 #include <linux/if_tun.h>
 
-int inline_c_System_IO_Tun_0_bd2a7fb9efe496bd2ce0895e72f4562981ffe9d7(char * path_inline_c_0) {
+int inline_c_System_IO_Tun_0_da36479f0ed2e2ec91e4b4ea08c8d4115a36d98d(const char * device_inline_c_0) {
 
         struct ifreq ifr;
-        int fd = -1;
+        int fd = 1;
 
-        printf("Got %s\n", path_inline_c_0);
+        if ((fd = open("/dev/net/tun", O_RDWR)) < 0) {
+            return -1;
+        }
+
+        memset(&ifr, 0, sizeof(ifr));
+        ifr.ifr_flags = IFF_TUN | IFF_NO_PI;
+        strncpy((char *)&ifr.ifr_name,
+                device_inline_c_0, 
+                sizeof(ifr.ifr_name) - 1);
+
+        if (ioctl(fd, TUNSETIFF, (void*)&ifr) < 0) {
+            close(fd);
+            return -1;
+        }
+
         return fd;
     
 }
--- lib/System/IO/Tun.hs
+++ lib/System/IO/Tun.hs
@@ -1,16 +1,35 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE QuasiQuotes     #-}
+-- | The function @openTun@ open a Linux Tun device and provides
+-- a @Handle@ to it for reading and writing.
+--
+-- Before calling @openTun@ it is expected that a Tun device, e.g. tun0,
+-- is created. E.g.:
+-- > sudo ip tuntap add mode tun tun0
+--
+-- Bring the new Tun device up, and then route some traffic to it.
+-- E.g. route all traffic to the 213.0.0.0/24 network to it:
+-- > sudo ip link set tun0 up
+-- > sudo ip route add 213.0.0.0/24 dev tun0
+--
+-- Now the Tun device can be opened. E.g.:
+-- > import System.IO (hGetContents)
+-- > Just h <- openTun "tun0"
+-- > hGetContents h
+--
+-- If some network data is sent to the network it will be printed to stdout.
+-- > ping 213.0.0.13
 module System.IO.Tun
     ( openTun
     ) where
 
 import Foreign.C.String (CString, withCString)
+import GHC.IO.Handle (Handle)
 import Language.C.Inline (CInt)
-import System.Posix.Types (Fd (..))
+import System.Posix (Fd (..), fdToHandle)
 
 import qualified Language.C.Inline as C
 
-C.include "<stdio.h>"
 C.include "<string.h>"
 C.include "<unistd.h>"
 C.include "<fcntl.h>"
@@ -18,18 +37,35 @@ C.include "<sys/ioctl.h>"
 C.include "<net/if.h>"
 C.include "<linux/if_tun.h>"
 
-openTun :: String -> IO (Maybe Fd)
-openTun path = do
-    fd <- withCString path openTunC
+-- | Open the named Tun device. If successfull a @Handle@ usable for
+-- reading and writing is returned.
+openTun :: String -> IO (Maybe Handle)
+openTun device = do
+    fd <- withCString device openTunC
     if fd < 0 then return Nothing
-              else return $ Just (Fd fd)
+              else Just <$> fdToHandle (Fd fd)
 
+-- | "inline-c" function to setup a Tun device.
 openTunC :: CString -> IO CInt
-openTunC path =
+openTunC device =
     [C.block| int {
         struct ifreq ifr;
-        int fd = -1;
+        int fd = 1;
 
-        printf("Got %s\n", $(char* path));
+        if ((fd = open("/dev/net/tun", O_RDWR)) < 0) {
+            return -1;
+        }
+
+        memset(&ifr, 0, sizeof(ifr));
+        ifr.ifr_flags = IFF_TUN | IFF_NO_PI;
+        strncpy((char *)&ifr.ifr_name,
+                $(const char* device), 
+                sizeof(ifr.ifr_name) - 1);
+
+        if (ioctl(fd, TUNSETIFF, (void*)&ifr) < 0) {
+            close(fd);
+            return -1;
+        }
+
         return fd;
     }|]
